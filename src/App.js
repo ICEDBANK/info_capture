f44637dca903bd802eb4e1646ae489f8602c1057
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-03 21:40:13 -0400

    Automated commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import './App.css';
 
 function App() {
 
+const handleSubmit = () => {
 
+  console.log('Form Submitted');
+
+};
 
   return (
     <>
 
-      div.App
+      <div className="App">
+        <div className="form-container">
+          <div>
+            <h2>Info Capture</h2>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <input type="text" name='firstname' placeholder='Enter Your First Name' />
+            <input type="text" name='firstname' placeholder='Enter Your First Name' />
+          </form>
+        </div>
+      </div>
 
     </>
   );
