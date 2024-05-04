fdf85991ce5307272ab483ed39a734b39d08c75f
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-03 22:07:12 -0400

    Automated commit

--- src/App.js
+++ src/App.js
@@ -17,15 +17,30 @@ const handleSubmit = () => {
             <h2>Info Capture</h2>
           </div>
           <form onSubmit={handleSubmit}>
-            <input type="text" name='firstname' placeholder='Enter Your First Name' />
-            <input type="text" name='lastname' placeholder='Enter Your Last Name' />
-            <input type="number" name='age' placeholder='Enter Your Age' />
-            <input type="text" name='email' placeholder='someone@somewhere.com' />
-            <input type="tel" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder='example: 123-456-7890' />
+            <input type="text" name='firstname' placeholder='Enter Your First Name' required />
+            <input type="text" name='lastname' placeholder='Enter Your Last Name' required />
+            <input type="number" name='age' placeholder='Enter Your Age' required />
+            <input type="text" name='city' placeholder='Enter Your city' required />
+            <input type="text" name='email' placeholder='someone@somewhere.com' required />
+            <input type="tel" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder='example: 123-456-7890' required />
           </form>
         </div>
       </div>
-
+      <div className="table-container">
+        <h2>User Data</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Age</th>
+              <th>City</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </>
   );
 }
